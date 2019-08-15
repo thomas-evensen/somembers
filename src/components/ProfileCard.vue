1ad107b79cfbcc1adb052cdc971c7d99280345cd
Thomas Evensen <thomas@dadabe.no>
2019-08-15 15:11:39 +0200

    Small DOM-cleaning

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -14,12 +14,17 @@
         :alt="`Bilde av ${item.node.name}`"
       />
     </g-link>
-    <div class="font-sans">
-      <h2 class="text-2xl mt-3 -mb-1 font-semibold cutText">
-        <g-link :to="item.node.slug" :aria-label="`Les mer om ${item.node.name}`">{{item.node.name}}</g-link>
-      </h2>
-      <h3 v-if="item.node.teamName" class="text-base cutText font-thin">{{ item.node.teamName }}</h3>
-    </div>
+
+    <g-link
+      class="block font-sans text-2xl mt-3 -mb-1 font-semibold cutText"
+      :to="item.node.slug"
+      :aria-label="`Les mer om ${item.node.name}`"
+    >{{item.node.name}}</g-link>
+
+    <div
+      v-if="item.node.teamName"
+      class="font-sans text-base cutText font-thin"
+    >{{ item.node.teamName }}</div>
   </div>
 </template>
 
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,13 +4,12 @@
       <g-link to="/" class="flex-1" aria-label="Gå til startsiden">
         <img class="w-32 md:w-40" src="../assets/logo.png" alt="SoCentral logo" />
       </g-link>
-      <div>
-        <a
-          class="bg-blue-700 text-white font-light py-2 px-4 rounded font-sans"
-          href="https://www.socentral.no/inkubator"
-          aria-label="Gå til SoCentral sin inkubatorside for å bli medlem"
-        >Bli medlem</a>
-      </div>
+
+      <a
+        class="bg-blue-700 text-white font-light py-2 px-4 rounded font-sans"
+        href="https://www.socentral.no/inkubator"
+        aria-label="Gå til SoCentral sin inkubatorside for å bli medlem"
+      >Bli medlem</a>
     </header>
     <slot />
     <div class="mx-auto mt-6 text-center">
