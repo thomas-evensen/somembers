1bb05b5efcc3345e83e07ec3064f2a7e12a9ef3d
Thomas Evensen <thomas@dadabe.no>
2020-07-20 13:20:39 +0200

    Configured Tailwind to use the in-house purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     theme: {
+        purge: [
+            './src/**/*.html',
+            './src/**/*.vue',
+            './src/**/*.jsx',
+        ],
         fontFamily: {
             'sans': ['Open Sans', '-apple-system', 'BlinkMacSystemFont']
         }
